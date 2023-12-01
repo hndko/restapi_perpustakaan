24b64bcc4c7d5f9ef15c7ad6715e025e6d96f133
Handoko <102887791+hndko@users.noreply.github.com>
2023-12-01 17:35:56 +0700

    update rest client

--- rest-client/app/Controllers/BooksController.php
+++ rest-client/app/Controllers/BooksController.php
@@ -2,13 +2,35 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\RequestInterface;
 use App\Controllers\BaseController;
 
 class BooksController extends BaseController
 {
     public function index()
     {
-        return view('books/index');
+        // Get the request instance
+        $request = service('request');
+
+        // Initialize HTTP client
+        $client = service('curlrequest');
+
+        // Send a GET request to the REST server endpoint
+        $response = $client->request('GET', 'http://localhost:8080/api/books');
+
+        // Check if the request was successful
+        if ($response->getStatusCode() === 200) {
+            // Parse the JSON response
+            $data = [
+                'books' => json_decode($response->getBody(), false)
+            ];
+
+            return view('books/index', $data);
+        } else {
+            // Handle the error, for example:
+            return 'Error fetching data from REST server';
+        }
     }
 
     public function show($id)
